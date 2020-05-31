21c692e62f5d4650c32e3393d027e11554cd5059
papadog <papadoginc@gmail.com>
2020-05-31 14:00:00 +0300

    add custom server

---
+++ client.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const next = require('next');
+
+const port = process.env.PORT || 3000;
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: 'client',
-      script: 'npm',
-      args: 'prod',
-    },
-  ],
-};
