9c8836fdb0972625b4d28dfa39dd0ebde007194e
papadog <papadoginc@gmail.com>
2020-06-12 11:55:51 +0300

    Refactor pagination component

--- components/UI/Pagination/Pagination.js
+++ components/UI/Pagination/Pagination.js
@@ -4,6 +4,10 @@ import styles from './Pagination.module.scss';
 const Pagination = ({ total, onPageChange, activePage = 1 }) => {
   if (!total) return null;
   const totalArray = [...Array(total).keys()];
+  if (total > 5) {
+    totalArray.splice(1, 0, 'dots');
+    totalArray.splice(totalArray.length - 1, 0, 'dots');
+  }
   const goBack = () => {
     if (Number(activePage) === 1) return;
     onPageChange(Number(activePage) - 1);
@@ -17,13 +21,43 @@ const Pagination = ({ total, onPageChange, activePage = 1 }) => {
       <li onClick={goBack} className={styles.item}>
         &#10092;
       </li>
-      {/* <li className={styles.dots}>...</li> */}
-      {totalArray.map((item) => {
+      {totalArray.map((item, i) => {
+        let display = 'flex';
+        switch (true) {
+          case i === 0 || i === totalArray.length - 1:
+            break;
+          case item === 'dots':
+            if (Math.abs(i - activePage) > 2) {
+              break;
+            } else {
+              display = 'none';
+            }
+          case Math.abs(i - activePage) > 1:
+            display = 'none';
+            break;
+          default:
+            break;
+        }
+        if (item === 'dots') {
+          return (
+            <li
+              style={{
+                display,
+              }}
+              className={styles.dots}
+            >
+              ...
+            </li>
+          );
+        }
         const pageNumber = item + 1;
 
         return (
           <li
             key={item}
+            style={{
+              display,
+            }}
             onClick={() => onPageChange(pageNumber)}
             className={`${styles.item} ${
               pageNumber === Number(activePage) ? styles.active : ''
@@ -33,7 +67,6 @@ const Pagination = ({ total, onPageChange, activePage = 1 }) => {
           </li>
         );
       })}
-      {/* <li className={styles.dots}>...</li> */}
       <li onClick={goForward} className={styles.item}>
         &#10093;
       </li>
--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const END_POINT =
-  process.env.NODE_ENV === 'production'
+  // process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'development'
     ? 'https://api.teplobrest.by/'
     : 'http://localhost:4000/';
 export const INITIAL_PAGE_SIZE = 12;
